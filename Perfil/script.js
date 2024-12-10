a71313711aaf78f910b0a5e0d6d1f061458cad1e
FelipehpSenai <153280436+FelipehpSenai@users.noreply.github.com>
2024-12-09 23:30:25 -0300

    Delete Perfil directory

--- Perfil/script.js
+++
@@ -1,9 +0,0 @@
-let currentIndex = 0;
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
-
-function changeSlide() {
-    currentIndex = (currentIndex + 1) % totalSlides;
-    document.querySelector('.slider').style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-setInterval(changeSlide, 5000);
